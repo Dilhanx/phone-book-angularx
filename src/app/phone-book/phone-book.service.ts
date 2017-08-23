4f2f3959f47368924686ffc9c0e167d59c3f7a7f
Dilhanx <dilhanx@outlook.com>
2017-08-23 12:10:35 +0530

    Fixed connections

--- src/app/phone-book/phone-book.service.ts
+++ src/app/phone-book/phone-book.service.ts
@@ -5,15 +5,15 @@ export class PhoneBookService {
 
   constructor(private htpp:Http) { }
   featchPhoneBook(){
-    return this.htpp.get("https://phone-book-sailsx.herokuapp.com").map(res=> res.json())
+    return this.htpp.get("https://phone-book-sailsx.herokuapp.com/contact").map(res=> res.json())
     
   }
   addPerson(values){
     console.log(JSON.stringify(values))
-    return this.htpp.put("https://phone-book-sailsx.herokuapp.com/",values).map(res=> res)
+    return this.htpp.put("https://phone-book-sailsx.herokuapp.com/contact/",values).map(res=> res)
   }
   deletePerson(values){
     console.log(JSON.stringify(values))
-    return this.htpp.delete("https://phone-book-sailsx.herokuapp.com/",new RequestOptions({body: values,})).map(res=> res)
+    return this.htpp.delete("https://phone-book-sailsx.herokuapp.com/contact/",new RequestOptions({body: values,})).map(res=> res)
   }
 }
